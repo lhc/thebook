b31fc3a8d8738c10a44dc2af3adcd096a68cdf15
Renne Rocha <renne@rennerocha.com>
2023-12-08 06:31:52 -0300

    Styling of transactions table and adding balance to results

--- src/moneybook/bookkeeping/views.py
+++ src/moneybook/bookkeeping/views.py
@@ -4,7 +4,19 @@ from moneybook.bookkeeping.models import Transaction
 
 
 def transactions(request):
-    transactions = Transaction.objects.all().order_by("date")
+    transactions = Transaction.objects.values(
+        "id",
+        "date",
+        "amount",
+        "description",
+        "account__name",
+        "category__name",
+    ).order_by("date")
+
+    transactions_balance = 0
+    for transaction in transactions:
+        transactions_balance += transaction["amount"]
+        transaction["balance"] = transactions_balance
 
     return render(
         request, "bookkeeping/transactions.html", context={"transactions": transactions}
