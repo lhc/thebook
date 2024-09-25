e22854d2d565b3a3758d950a0f48375acda47808
Renne Rocha <renne@rocha.dev.br>
2024-09-24 21:16:44 -0300

    More improvements in transaction categorizer

--- src/thebook/categorizer.py
+++ src/thebook/categorizer.py
@@ -7,9 +7,10 @@ RECURRING_DONATION = "Doação Recorrente"
 BANK_FEES = "Tarifas Bancárias"
 BANK_INCOME = "Investimentos"
 RECURRING = "Recorrente"
-MEMBERSHIP_FEE = "Mensalidades"
+MEMBERSHIP_FEE = "Mensalidade"
 CASH_BOOK_TRANSFER = "Transferência entre contas"
 ACCOUNTANT = "Contabilidade"
+TAXES = "Impostos"
 
 
 def categorize():
@@ -23,6 +24,8 @@ def categorize():
     recurring, _ = Category.objects.get_or_create(name=RECURRING)
     cash_book_transfer, _ = Category.objects.get_or_create(name=CASH_BOOK_TRANSFER)
     accountant, _ = Category.objects.get_or_create(name=ACCOUNTANT)
+    taxes, _ = Category.objects.get_or_create(name=TAXES)
+    membership_fee, _ = Category.objects.get_or_create(name=MEMBERSHIP_FEE)
 
     for transaction in Transaction.objects.filter(category=uncategorized):
         if "TARIFA BANCARIA" in transaction.description:
@@ -46,6 +49,44 @@ def categorize():
             in transaction.description
         ):
             transaction.category = cash_book_transfer
+        elif (
+            "TED-TRANSF ELET DISPON REMET.PAYPAL DO BRASIL INS"
+            in transaction.description
+        ):
+            transaction.category = cash_book_transfer
+        elif "TRANSFERENCIA PIX REM: PAYPAL DO BRASIL INST" in transaction.description:
+            transaction.category = cash_book_transfer
+        elif (
+            "Transferência recebida - Paypal Do Brasil Instituicao De Pagamento Ltda"
+            in transaction.description
+        ):
+            transaction.category = cash_book_transfer
+        elif (
+            "Transf Pix recebida - PAYPAL DO BRASIL INSTITUICAO DE PAGAMENTO LTDA"
+            in transaction.description
+        ):
+            transaction.category = cash_book_transfer
+        elif (
+            "PAGTO ELETRONICO TRIBUTO INTERNET --P.M CAMPINAS/SP"
+            in transaction.description
+        ):
+            transaction.category = taxes
+            transaction.notes = "ISSQN"
+        elif (
+            "TRANSFERENCIA PIX REM: ELITON P CRUVINEL" in transaction.description
+            and transaction.amount == 75
+        ):
+            transaction.category = membership_fee
+        elif (
+            "TRANSFERENCIA PIX REM: ELITON P CRUVINEL" in transaction.description
+            and transaction.amount == 85
+        ):
+            transaction.category = membership_fee
+        elif (
+            "TRANSFERENCIA PIX REM: ESTEVAN CASTILHO DE M" in transaction.description
+            and transaction.amount == 85
+        ):
+            transaction.category = membership_fee
         elif 0 < transaction.amount < 50:
             transaction.category = donation
 
