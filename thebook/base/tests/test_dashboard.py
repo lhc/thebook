791dedde9535f16ef842fa84c6d75c45fdad7330
Renne Rocha <renne@rocha.dev.br>
2024-10-01 10:22:07 -0300

    Return balance summary in dashboard context

--- thebook/base/tests/test_dashboard.py
+++ thebook/base/tests/test_dashboard.py
@@ -1,10 +1,16 @@
+import datetime
+from decimal import Decimal
 from http import HTTPStatus
+
+import pytest
+from model_bakery import baker
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from model_bakery import baker
+from thebook.base.views import _get_dashboard_context
+from thebook.bookkeeping.models import CashBook, Transaction
 
 
 def test_unauthenticated_access_to_dashboard_redirect_to_login_page(db, client):
@@ -23,3 +29,30 @@ def test_allowed_access_dashboard_authenticated(db, client):
     response = client.get(reverse("base:dashboard"))
 
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.freeze_time("2024-09-15")
+def test_dashboard_context(db):
+    cash_book_1 = CashBook.objects.create(name="Cash Book 1")
+    cash_book_2 = CashBook.objects.create(name="Cash Book 2")
+
+    # fmt: off
+    baker.make(Transaction, cash_book=cash_book_1, date=datetime.date(2023, 11, 1), amount=Decimal("100"))
+    baker.make(Transaction, cash_book=cash_book_1, date=datetime.date(2024, 9, 1), amount=Decimal("12.53"))
+    baker.make(Transaction, cash_book=cash_book_1, date=datetime.date(2024, 9, 10), amount=Decimal("-15.5"))
+    baker.make(Transaction, cash_book=cash_book_1, date=datetime.date(2024, 9, 12), amount=Decimal("19.23"))
+
+    baker.make(Transaction, cash_book=cash_book_2, date=datetime.date(2023, 11, 1), amount=Decimal("98.11"))
+    baker.make(Transaction, cash_book=cash_book_2, date=datetime.date(2024, 9, 1), amount=Decimal("15.4"))
+    baker.make(Transaction, cash_book=cash_book_2, date=datetime.date(2024, 9, 10), amount=Decimal("42.58"))
+    baker.make(Transaction, cash_book=cash_book_2, date=datetime.date(2024, 9, 12), amount=Decimal("-58.39"))
+    # fmt: on
+
+    context = _get_dashboard_context()
+
+    assert context == {
+        "deposits": Decimal("89.74"),
+        "withdraws": Decimal("-73.89"),
+        "balance": Decimal("15.85"),
+        "overall_balance": Decimal("213.96"),
+    }
--- thebook/base/views.py
+++ thebook/base/views.py
@@ -1,15 +1,32 @@
 import datetime
-from decimal import Decimal
+from decimal import Decimal, ROUND_UP
 
 from django.shortcuts import render
 
+from thebook.bookkeeping.models import CashBook
+
 
 def _get_dashboard_context():
+    deposits = Decimal("0")
+    withdraws = Decimal("0")
+    balance = Decimal("0")
+    overall_balance = Decimal("0")
+
+    today = datetime.date.today()
+    cash_books = CashBook.objects.summary(year=today.year, month=today.month)
+    for cash_book in cash_books:
+        deposits += cash_book.deposits
+        withdraws += cash_book.withdraws
+        balance += cash_book.balance
+        overall_balance += cash_book.overall_balance
+
     return {
-        "deposits": Decimal("0"),
-        "withdraws": Decimal("0"),
-        "balance": Decimal("0"),
-        "overall_balance": Decimal("0"),
+        "deposits": deposits.quantize(Decimal(".01"), rounding=ROUND_UP),
+        "withdraws": withdraws.quantize(Decimal(".01"), rounding=ROUND_UP),
+        "balance": balance.quantize(Decimal(".01"), rounding=ROUND_UP),
+        "overall_balance": overall_balance.quantize(
+            Decimal(".01"), rounding=ROUND_UP
+        ),
     }
 
 
