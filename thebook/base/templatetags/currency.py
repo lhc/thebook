10e1dfd01de3c7c9523797cb41e1ac0863459c94
Renne Rocha <renne@rocha.dev.br>
2024-10-16 11:21:04 -0300

    Improve number string formatting to improve readability in templates

--- thebook/base/templatetags/currency.py
+++ thebook/base/templatetags/currency.py
@@ -7,6 +7,20 @@ register = template.Library()
 
 @register.filter
 def money(value):
-    if isinstance(value, numbers.Number):
-        return f"(R$ {-1 * value:.2f})" if value < 0 else f"R$ {value:.2f}"
-    return "-"
+    if not isinstance(value, numbers.Number):
+        return "-"
+
+    integer_value = str(abs(int(value)))
+    integer_part = ".".join(
+        [
+            integer_value[::-1][i : i + 3][::-1]
+            for i in range(0, len(integer_value), 3)
+        ][::-1]
+    )
+    decimal_part = int((abs(value) % 1) * 100)
+
+    return (
+        f"(R$ {integer_part},{decimal_part:02})"
+        if value < 0
+        else f"R$ {integer_part},{decimal_part:02}"
+    )
