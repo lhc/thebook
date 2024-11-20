5659eeee6df902c8d087821c9c50a4208f98e5b2
Renne Rocha <renne@rocha.dev.br>
2024-11-20 08:58:34 -0300

    Provide correct URL to be redirected after login

--- thebook/settings.py
+++ thebook/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "users.User"
 
-LOGIN_REDIRECT_URL = reverse_lazy("dashboard")
+LOGIN_REDIRECT_URL = reverse_lazy("base:dashboard")
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
