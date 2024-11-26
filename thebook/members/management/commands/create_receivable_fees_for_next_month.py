9b77c30ac28e9e846747142a0c75d7334089b1af
Renne Rocha <renne@rocha.dev.br>
2024-11-25 22:28:12 -0300

    Command to create missing receivable fees for next month

---
+++ thebook/members/management/commands/create_receivable_fees_for_next_month.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+from thebook.members.models import ReceivableFee
+
+
+class Command(BaseCommand):
+    help = "Create receivable fees for next month"
+
+    def handle(self, *args, **options):
+        ReceivableFee.objects.create_for_next_month()
+
+        self.stdout.write(
+            self.style.SUCCESS("Successfully created receivable fees for next month")
+        )
