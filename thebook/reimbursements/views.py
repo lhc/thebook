6fcf22072b846247f27bb592d723648a042a057b
Renne Rocha <renne@rocha.dev.br>
2025-04-07 13:49:53 -0300

    Reimbursement request form

--- thebook/reimbursements/views.py
+++ thebook/reimbursements/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from thebook.reimbursements.forms import CreateReimbursementRequestForm
+
+
+def create_reimbursement(request):
+    if request.method == "POST":
+        form = CreateReimbursementRequestForm(request.POST)
+        if form.is_valid():
+            return HttpResponse("Reimbursement request received!")
+    else:
+        form = CreateReimbursementRequestForm()
+
+    return render(request, "reimbursements/create.html", context={"form": form})
