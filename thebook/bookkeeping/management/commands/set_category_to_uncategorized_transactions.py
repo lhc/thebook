bc1983fb0588dbbb10751e6720660fec9b9b2433
Renne Rocha <renne@rocha.dev.br>
2025-05-15 18:18:15 -0300

    Add management command to categorize transactions

---
+++ thebook/bookkeeping/management/commands/set_category_to_uncategorized_transactions.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from thebook.bookkeeping.models import CategoryMatchRule, Transaction
+
+
+class Command(BaseCommand):
+    help = "Automatically set a category for uncategorized transactions"
+
+    def handle(self, *args, **options):
+        uncategorized_transactions = Transaction.objects.filter(category__isnull=True)
+        category_match_rules = CategoryMatchRule.objects.order_by("priority")
+
+        for transaction in uncategorized_transactions:
+            transaction.categorize(rules=category_match_rules)
+
+        self.stdout.write(self.style.SUCCESS("Successfully categorized transactions"))
