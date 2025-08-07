6ed483e81e5dbb50a49d42ddb7903a4f323acd48
Renne Rocha <renne@rocha.dev.br>
2025-08-07 19:19:18 -0300

    Update receivable fees with matching transactions

---
+++ thebook/members/management/commands/match_receivable_fees_with_transactions.py
@@ -0,0 +1,25 @@
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+
+from thebook.members.models import FeePaymentStatus, ReceivableFee
+
+
+class Command(BaseCommand):
+    help = "Match unpaid receivable fees with transactions"
+
+    def handle(self, *args, **options):
+        for status in (FeePaymentStatus.DUE, FeePaymentStatus.UNPAID):
+            receivable_fees = ReceivableFee.objects.filter(status=status)
+            for receivable_fee in receivable_fees:
+                # This is the date we started to create ReceivableFees
+                # automatically, so we should ignore past transactions
+                transaction = Transaction.objects.filter(
+                    date__gte=datetime.date(2025, 6, 1)
+                ).find_match_for(receivable_fee)
+                if not transaction:
+                    continue
+                receivable_fee.paid_with(transaction)
+
+        self.stdout.write(self.style.SUCCESS("Successfully matched transactions"))
