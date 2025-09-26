1aa33ce5c0826ecf87f61ac56f0aa3b0d169ddbb
Renne Rocha <renne@rocha.dev.br>
2025-09-26 17:51:27 -0300

    Add OpenPixPayload model to Django admin

--- thebook/webhooks/admin.py
+++ thebook/webhooks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from thebook.webhooks.models import OpenPixWebhookPayload
+
+
+@admin.register(OpenPixWebhookPayload)
+class OpenPixWebhookPayloadAdmin(admin.ModelAdmin): ...
