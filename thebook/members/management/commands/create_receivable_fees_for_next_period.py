f0d6e0275f63dc402c0672a02c4b5ecaf8dde497
Renne Rocha <renne@rocha.dev.br>
2025-10-27 09:47:33 -0300

    Fix typo in method name call

--- thebook/members/management/commands/create_receivable_fees_for_next_period.py
+++ thebook/members/management/commands/create_receivable_fees_for_next_period.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = "Create receivable fees for next period"
 
     def handle(self, *args, **options):
-        ReceivableFee.objects.create_for_next_iperiod()
+        ReceivableFee.objects.create_for_next_period()
 
         self.stdout.write(
             self.style.SUCCESS("Successfully create d receivable fees for next period")
