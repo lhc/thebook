aa58a7528ccb6eb098c49fce83282de18c4c8e9b
Renne Rocha <renne@rocha.dev.br>
2025-12-05 20:11:41 -0300

    AUtomatically read Cora OFX emails

---
+++ thebook/bookkeeping/management/commands/process_cora_email.py
@@ -0,0 +1,48 @@
+import email
+import imaplib
+import io
+
+from decouple import config
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+
+from thebook.bookkeeping.importers import import_transactions
+from thebook.bookkeeping.models import CashBook
+
+
+class Command(BaseCommand):
+    help = "Check inbox for Cora OFX files and import them"
+
+    def handle(self, *args, **options):
+        cora_cash_book = CashBook.objects.get(name="Cora")
+        User = get_user_model()
+
+        # TODO: Create a user for automated imports
+        user = User.objects.get(email="renne@rocha.dev.br")
+
+        M = imaplib.IMAP4_SSL(config("CORA_EMAIL_HOST_IMAP"))
+        M.login(config("CORA_EMAIL_HOST_USER"), config("CORA_EMAIL_HOST_PASSWORD"))
+        M.select(mailbox="INBOX")
+
+        typ, msgnums = M.search(None, "FROM", "naoresponda@cora.com.br")
+
+        for msgnum in msgnums[0].split():
+            typ, data = M.fetch(msgnum, "(RFC822)")
+            raw_email_string = data[0][1].decode("utf-8")
+            message = email.message_from_string(raw_email_string)
+
+            for part in message.walk():
+                is_attachment = part.get_filename()
+                if is_attachment:
+                    attachment_content = part.get_payload(decode=True)
+                    ofx_file = io.StringIO(attachment_content.decode("utf-8"))
+                    import_transactions(
+                        ofx_file, "ofx", cora_cash_book, user, None, None
+                    )
+
+                    M.copy(msgnum, "CoraProcessed")
+                    M.store(msgnum, "+FLAGS", "\\Deleted")
+
+        M.close()
+        M.logout()
