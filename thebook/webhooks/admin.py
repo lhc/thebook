d1fb0cee6645ee8614920181ebcbb61662512c2d
Renne Rocha <renne@rocha.dev.br>
2025-12-08 19:21:55 -0300

    Allow to process an OpenPix payload manually

--- thebook/webhooks/admin.py
+++ thebook/webhooks/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from thebook.webhooks.models import OpenPixWebhookPayload
 
 
+@admin.action(description="Process Webhook Payload")
+def process_payload(modeladmin, request, queryset):
+    for payload in queryset:
+        payload.process()
+
+
 @admin.register(OpenPixWebhookPayload)
 class OpenPixWebhookPayloadAdmin(admin.ModelAdmin):
+    actions = [
+        process_payload,
+    ]
     list_display = [
         "status",
         "created_at",
