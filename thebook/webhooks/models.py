a90a9f0f43d76262b5d62f521b1c7f16ddda28d7
Renne Rocha <renne@rocha.dev.br>
2025-12-08 16:53:43 -0300

    Create status field to track OpenPix Webhook payload processing

--- thebook/webhooks/models.py
+++ thebook/webhooks/models.py
@@ -1,7 +1,27 @@
 from django.db import models
+from django.utils.functional import classproperty
+from django.utils.translation import gettext as _
+
+
+class ProcessingStatus:
+    RECEIVED = 1
+    PROCESSED = 2
+
+    @classproperty
+    def choices(cls):
+        return (
+            (cls.RECEIVED, _("Received")),
+            (cls.PROCESSED, _("Processed")),
+        )
 
 
 class OpenPixWebhookPayload(models.Model):
     thebook_token = models.CharField()
     payload = models.CharField()
+    status = models.IntegerField(
+        choices=ProcessingStatus.choices,
+        default=ProcessingStatus.RECEIVED,
+        verbose_name=_("Processing Status"),
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
