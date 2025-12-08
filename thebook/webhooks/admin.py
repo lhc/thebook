09664a7c0aecb2738d04c6067ac00e5f683f4ac1
Renne Rocha <renne@rocha.dev.br>
2025-12-08 19:18:02 -0300

    Improve admin to handle OpenPix webhooks payloads

--- thebook/webhooks/admin.py
+++ thebook/webhooks/admin.py
@@ -4,4 +4,11 @@ from thebook.webhooks.models import OpenPixWebhookPayload
 
 
 @admin.register(OpenPixWebhookPayload)
-class OpenPixWebhookPayloadAdmin(admin.ModelAdmin): ...
+class OpenPixWebhookPayloadAdmin(admin.ModelAdmin):
+    list_display = [
+        "status",
+        "created_at",
+    ]
+    list_filter = [
+        "status",
+    ]
