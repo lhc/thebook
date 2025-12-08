83fd7c7666571de0b9c5c2600737769e2bde01b8
Renne Rocha <renne@rocha.dev.br>
2025-12-08 19:15:18 -0300

    Create function to process OpenPix webhook payloads creating transactions

--- thebook/webhooks/models.py
+++ thebook/webhooks/models.py
@@ -1,6 +1,14 @@
-from django.db import models
+import datetime
+import json
+
+import jmespath
+
+from django.contrib.auth import get_user_model
+from django.db import DatabaseError, models, transaction
 from django.utils.functional import classproperty
 from django.utils.translation import gettext as _
+
+from thebook.bookkeeping.models import CashBook, Transaction
 
 
 class ProcessingStatus:
@@ -25,3 +33,70 @@ class OpenPixWebhookPayload(models.Model):
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def process(self, cash_book=None, user=None):
+        if self.status == ProcessingStatus.PROCESSED:
+            return
+
+        if cash_book is None:
+            cash_book = CashBook.objects.get(name="OpenPix")
+
+        if user is None:
+            user = get_user_model().objects.get(email="renne@rocha.dev.br")
+
+        payload = json.loads(self.payload)
+
+        if jmespath.search("event", payload) != "OPENPIX:TRANSACTION_RECEIVED":
+            return
+
+        amount = jmespath.search("pix.charge.value || pix.value", payload) / 100
+
+        raw_fee = jmespath.search("pix.charge.fee", payload)
+        if not raw_fee:
+            raw_fee = round(amount * 100 * 0.0080, 2)
+        fee = (-1 * raw_fee) / 100
+
+        # Original in UTC time
+        paid_at = jmespath.search("pix.charge.paidAt || pix.time", payload)
+        utc_transaction_date = datetime.datetime.strptime(
+            paid_at, "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+
+        comment = jmespath.search("pix.charge.comment", payload) or ""
+        payer_name = (
+            jmespath.search("pix.charge.payer.name || pix.payer.name", payload) or ""
+        )
+        payer_tax_id = (
+            jmespath.search(
+                "pix.charge.payer.taxID.taxID || pix.payer.taxID.taxID", payload
+            )
+            or ""
+        )
+        description = " - ".join(
+            [part for part in (comment, payer_name, payer_tax_id) if part]
+        )
+
+        reference = jmespath.search(
+            "pix.charge.transactionID || pix.transactionID", payload
+        )
+
+        with transaction.atomic():
+            Transaction.objects.create(
+                reference=reference,
+                date=utc_transaction_date,
+                description=description,
+                amount=amount,
+                cash_book=cash_book,
+                created_by=user,
+            )
+            Transaction.objects.create(
+                reference="T" + reference,
+                date=utc_transaction_date,
+                description="Taxa - " + description,
+                amount=fee,
+                cash_book=cash_book,
+                created_by=user,
+            )
+
+            self.status = ProcessingStatus.PROCESSED
+            self.save()
