46f482dd0e2a590c0ec9eb104e5435c6581198df
Renne Rocha <renne@rocha.dev.br>
2026-03-02 16:16:07 -0300

    Fetch OpenPix transactions when available

---
+++ thebook/webhooks/management/commands/fetch_openpix_transactions.py
@@ -0,0 +1,18 @@
+import datetime
+
+from django.core.management.base import BaseCommand
+
+from thebook.webhooks.openpix.services import fetch_transactions
+
+
+class Command(BaseCommand):
+    help = "Fetch all transactions in the last 2 days"
+
+    def handle(self, *args, **options):
+        """If we miss some webhook payload, this command should ensure that we don't lose any transaction"""
+        end_date = datetime.date.today()
+        start_date = end_date - datetime.timedelta(days=2)
+
+        transactions = fetch_transactions(start_date, end_date)
+        for transaction in transactions:
+            transaction.save()
